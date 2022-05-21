ac0c9e378826f14a2f714cff0aaca9f871388670
kyojro <rostik.kuharchuk@gmail.com>
2022-05-22 00:11:32 +0300

    second commit

--- client.py
+++ client.py
@@ -3,9 +3,27 @@ import psutil
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 2000))
-i = 0
-while True:
-    while i < 1:
-        i += 1
-        client.send(f"cpu = {psutil.cpu_percent(4)}".encode("utf-8"))
-        client.send(f"ram = {psutil.virtual_memory()[2]}".encode("utf-8"))
+
+
+class Client:
+    def __init__(self, opt_1, opt_2):
+        self.client = socket.socket(opt_1, opt_2)
+
+    def connect_serv(self, ip, port):
+        self.client.connect((ip, port))
+
+    @staticmethod
+    def delivery():
+        i = 0
+        while True:
+            data = client.recv(1024).decode("utf-8").lower()
+            print(data)
+            while i < 1:
+                i += 1
+                client.send(f"ram = '{psutil.virtual_memory()[2]}'".encode("utf-8"))
+                client.send(f"cpu = '{psutil.cpu_percent(4)}'".encode("utf-8"))
+
+
+x = Client(socket.AF_INET, socket.SOCK_STREAM)
+x.connect_serv("127.0.0.1", 2000)
+x.delivery()
--- server.py
+++ server.py
@@ -1,17 +1,34 @@
 import socket
 import logging
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 2000))
-server.listen(10)
 
-while True:
-    user, address = server.accept()
-    print("connect ", user.getpeername())
-    user.send("connect".encode("utf-8"))
-    while True:
-        data = user.recv(1024).decode("utf-8").lower()
-        print("user_send: ", data)
-        if float(data[7]) > 70:
-            logging.basicConfig(level=logging.INFO, filename='info.log', filemode='a',
-                                format='%(levelname)s - %(message)s')
-            logging.info(f'user: {user} CPU usage: {data[2]} \n')
+
+class Server:
+    def __init__(self, opt_1, opt_2):
+        # socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.user = None
+        self.server = socket.socket(opt_1, opt_2)
+
+    def connect_server(self, ip, port, count_l):
+        # "127.0.0.1", 2000
+        self.server.bind((ip, port))
+        # 10
+        self.server.listen(count_l)
+
+    def work_server(self):
+        while True:
+            self.user, address = self.server.accept()
+            print("connect ", self.user.getpeername())
+            self.user.send("connect".encode("utf-8"))
+            while True:
+                data = self.user.recv(1024).decode("utf-8").lower()
+                print("user_send: ", data)
+                if "cpu" in data and float(data.split("'")[1]) > 0.5:
+                    logging.basicConfig(level=logging.INFO, filename='info.log', filemode='w',
+                                        format='%(levelname)s - %(message)s')
+                    d = data.split("'")[1]
+                    logging.info(f"user: {self.user} CPU usage: {d}")
+
+
+server = Server(socket.AF_INET, socket.SOCK_STREAM)
+server.connect_server("127.0.0.1", 2000, 10)
+server.work_server()
